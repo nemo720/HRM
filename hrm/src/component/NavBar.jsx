72f226d26d6ec6fa963b7a8af3cf9d89ab0e40ac
nemo720 <quandoan.140203@gmail.com>
2024-05-01 23:31:27 +0700

    fixLogo

--- hrm/src/component/NavBar.jsx
+++ hrm/src/component/NavBar.jsx
@@ -2,7 +2,7 @@ import NavItem from "./NavItem";
 import { Home, Users, BellRing,HandCoins } from "lucide-react";
 // chung 1 thu muc la tro thang
 // khac thu muc la .. de thoat, 1 cham la thoat 1 lan
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/Logo.jpg";
 
 const navContent = [
   {
@@ -29,8 +29,8 @@ const navContent = [
 export const NavBar = () => {
   return (
     <div className="max-w-full w-1/5 bg-white-400 border-2 flex items-center flex-col ">
-      <div className=" w-[70%] h-[122px] mt-6 bg-white-100 ">
-        <img src={Logo} alt="Logo" className="w-full h-full top-1/2" />
+      <div className=" w-[70%] h-[150px] mt-6 bg-white-100 ">
+        <img src={Logo} alt="Logo" className="w-full h-auto px-3" />
       </div>
       {navContent.map((nav) => (
         <NavItem key={nav.title} title={nav.title} href={nav.href} >
